fe2c52552d4874889d9b752bb6113d668520d12a
Mafhz <martinn.henriquez@gmail.com>
2020-08-15 18:22:00 -0500

    Modo oscuro en pagina Mis Gifos

--- js/app.js
+++ js/app.js
@@ -93,5 +93,6 @@ const btnSwitch = document.querySelector('#switch');
 
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
+    // document.body.style.background = 'red';
     btnSwitch.classList.toggle('active');
 })
