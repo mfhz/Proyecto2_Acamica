0ca2c58db20fc01b7587ef5bf952fa101a3f2860
Mafhz <martinn.henriquez@gmail.com>
2020-08-15 19:27:13 -0500

    Agregando localStorage para el modo oscuro

--- js/app.js
+++ js/app.js
@@ -94,5 +94,21 @@ const btnSwitch = document.querySelector('#switch');
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     // document.body.style.background = 'red';
-    btnSwitch.classList.toggle('active');    
+    btnSwitch.classList.toggle('active');
+    
+    //Guardar el modo en LocalStorage
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');        
+    }
 })
+
+// Se obtiene el modo actual
+if (localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+} else {
+    document.body.classList.remove('dark');    
+    btnSwitch.classList.remove('active');
+}
