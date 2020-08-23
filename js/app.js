0329752e19e8f0dee4927f2706330f3c995c2039
Mafhz-Desktop <martinn.henriquez@gmail.com>
2020-08-23 12:49:19 -0500

    Parte logica buscador principal (Busqueda satisfactoria)

--- js/app.js
+++ js/app.js
@@ -119,9 +119,153 @@ menuNav.addEventListener('click', () => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Imprimiendo Gifs si se encuentra en la busqueda principal del proyecto
-const obtenerInput = document.querySelector('#busqueda');
-const imprimirBusqueda = document.querySelector('#btnSearch');
-imprimirBusqueda.addEventListener('click', ev => {
-    console.log('CLICK')
-});
+const line = document.querySelector('#line');
+const sectionGifs = document.querySelector('.interactive-search')
+const caja1 = document.querySelector('#box-search');
+const btn =document.querySelector('#btnSearch');
+const impresionError = document.querySelector('#error');
+const titulo = document.querySelector('#titleError');
+btn.addEventListener('click', obtenerUsuario);
+const api_key = 'aSfyIm4TLkXUqhZxWhGqVaDndjyX8PBd';
+const num = 0;
+function obtenerApi(user) {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${user}&api_key=${api_key}&limit=25`;
+
+    fetch(url)
+        .then((success) => {
+            // console.log(success);
+            if (success.ok) {
+                return success.json();
+            } else {
+                throw new Error(('success') + ' no comunica con la API');
+            }
+            
+
+        })
+        .then((data) => {
+            // console.log(data.data);
+            // console.log(data.data[0].images.downsized.url);
+            // console.log(data.meta);
+            line.classList.remove('error');
+            sectionGifs.classList.remove('error');
+            impresionError.classList.add('error');
+            if (data.data.length === 0) {
+                // console.log('ERROR')
+                throw new Error(' (data) No existe valor ingresado por el usuario');
+            } else {                
+                caja1.classList.add('box-search');                
+
+                // console.log(caja.children.length);                
+                if (caja1.children.length > 0) {
+                    // console.log(caja.childNodes);
+                    for (let i = 0; i < 25; i++) {
+                        // console.log('SI');
+                        caja1.removeChild(caja1.firstChild);
+                                                
+                    }
+                    // console.log(caja.childNodes);                        
+                }
+
+                titulo.innerText = user;
+                
+                data.data.forEach(function(el) {                
+                    // console.log(el.images.downsized.url);                    
+                    const image = document.createElement('img');
+                    // const title = document.createElement('h2');
+                    const boxGif = document.createElement('div');
+
+                    image.src = el.images.downsized.url;
+                    image.classList.add('imgGif');
+                    
+                    // title.textContent = el.title;
+                    
+                    boxGif.appendChild(image);
+                    // boxGif.appendChild(title);
+                    caja1.insertAdjacentElement('afterbegin', boxGif);
+                }); 
+
+
+                
+            }
+
+        })
+        .catch((err) => {
+            // console.log(`${err}`);
+            // console.log(caja.childNodes);
+
+            // console.log(impresionError.childNodes);
+
+            impresionError.classList.remove('error');
+
+            if (caja.childNodes.length === 0) {
+                // console.log('si está vacio');
+                // console.log(user);
+                // console.log(impresionError.children);
+                
+
+                if (impresionError.children.length === 1) {
+                    
+                    const errorBox = document.createElement('div');
+                    const errorImg = document.createElement('img');
+                    const errorText = document.createElement('p');
+
+                    
+                    
+                    titulo.innerText = user;
+                    errorImg.setAttribute('src', './icon-busqueda-sin-resultado.svg')
+                    errorText.innerText = 'Intenta con otra búsqueda.';
+                    
+                    
+                    
+                    impresionError.appendChild(titulo);
+                    impresionError.appendChild(errorImg);
+                    impresionError.appendChild(errorText);
+        
+        
+                    // image.src = el.images.downsized.url;
+                    // image.classList.add('imgGif');
+                } else {
+                    titulo.innerText = user;
+                    impresionError.insertAdjacentElement('afterbegin', titulo);
+                }
+
+
+            } else {
+                console.log('no entra')
+                for (let i = 0; i < 25; i++) {
+                    // console.log('SI');
+                    caja.removeChild(caja.firstChild);
+                                            
+                }
+                titulo.innerText = user;
+                impresionError.insertAdjacentElement('afterbegin', titulo);
+            }
+        })
+}
+
+
+
+
+
+function obtenerUsuario() {
+    // console.log('click imagen')
+    const info = document.querySelector('#busqueda').value;
+    // console.log(info);
+    obtenerApi(info);
+}
