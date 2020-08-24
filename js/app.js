4b8233bc6ee646998e4059fcd855c577cab852f3
Mafhz-Desktop <martinn.henriquez@gmail.com>
2020-08-24 00:17:26 -0500

    Se corrige ventana modal version mobile y desktop

--- js/app.js
+++ js/app.js
@@ -6,83 +6,11 @@ let text;
 let cerrar, fav, descarga;
 const btnSwitch = document.querySelector('#switch');
 const menuNav = document.querySelector('.menu');
+const boxtrending = document.querySelector('.box-zoom-img');
 
 
 
-//Ventana Modal
-imagen.forEach(re => {
-    re.addEventListener('click', function(ev, index) {
-        // console.log('click')
-        // var altovent = window.screen.height;
-        // console.log(altovent);
-        console.log(re[0]);
-        document.getElementsByTagName("html")[0].style.overflow = "hidden";
-        // console.log(re.parentNode);
-        caja.style.height = '2000' + "px";
-        caja.classList.remove('caja');
-        caja.classList.add('caja-off');
-        contenedor = document.createElement('div');
-        caja.appendChild(contenedor);
-        contenedor.classList.add('zoom');
-        cerrar = document.createElement('img');
-        cerrar.setAttribute('src', './images/close.svg');
-        cerrar.setAttribute('class', 'close');
-        contenedor.appendChild(cerrar);
-        contenedor.appendChild(re);
-        contenedor5 = document.createElement('div');
-        contenedor5.classList.add('container');
-        contenedor.appendChild(contenedor5);
-        contenedor3 = document.createElement('div');
-        contenedor3.classList.add('container-text');
-        contenedor5.appendChild(contenedor3)
-        text = document.createElement('p')
-        contenedor3.appendChild(text);
-        text = document.createElement('h3')
-        contenedor3.appendChild(text);
-        contenedor2 = document.createElement('div');
-        contenedor2.classList.add('container-img');
-        contenedor5.appendChild(contenedor2);
-        fav = document.createElement('img');
-        fav.setAttribute('src', './images/icon-fav-active.svg');
-        fav.setAttribute('class', 'img-fav');
-        contenedor2.appendChild(fav);
-        contenedor4 = document.createElement('div');
-        contenedor4.classList.add('box-img-download');
-        contenedor2.appendChild(contenedor4);
-        descarga = document.createElement('img');
-        descarga.setAttribute('src', './images/icon-download.svg');
-        descarga.setAttribute('class', 'img-download');
-        contenedor4.appendChild(descarga);
 
-
-
-        re.style.pointerEvents = "none";
-        
-
-        
-        
-        
-        
-
-        cerrar.addEventListener('click', function(ev){
-            ev.preventDefault();
-            // console.log('cerrar');
-            // console.log(this.parentNode.parentNode);
-            // // console.log(this.parentNode);
-            // console.log(re);
-            // console.log(re.parentNode);
-            document.getElementsByTagName("html")[0].style.overflow = "auto";
-            caja.classList.remove('caja-off');
-            caja.classList.add('caja');
-            re.style.pointerEvents = 'auto';
-            padreImg.appendChild(re);
-            // console.log(re);
-            // console.log(re.parentNode);                    
-            this.parentNode.remove();
-        })
-        
-    })    
-})
 
 
 
@@ -294,7 +222,7 @@ function obtenerUsuario() {
 
 // Mostrar Trending en pantalla principal
 document.addEventListener('DOMContentLoaded', obtenerTrending);
-const boxtrending = document.querySelector('.box-zoom-img');
+
 
 
 function obtenerTrending() {
@@ -313,16 +241,125 @@ function obtenerTrending() {
             // console.log(data.data);
 
             data.data.forEach((el) => {
-                // console.log(el);                    
-                    const image = document.createElement('img');
-                    image.src = el.images.downsized.url;
-                    image.classList.add('zoom-img');
-                    // boxtrending.insertAdjacentElement('afterbegin', image);
-                    boxtrending.appendChild(image);
+                // console.log(el);                                    
+                const imageBox = document.createElement('div');
+                boxtrending.appendChild(imageBox);
+                const image = document.createElement('img');
+                image.src = el.images.downsized.url;
+                image.classList.add('zoom-img');
+                imageBox.appendChild(image);
+                // imageBox.appendChild(imagen)
+                // boxtrending.insertAdjacentElement('afterbegin', image);
             })
         })
         .catch((err) => {
             console.log(`${err}`);
         })
     
+}
+
+
+
+
+
+
+//Ventana Modal
+boxtrending.addEventListener('click', ventanaModal);
+
+function ventanaModal(e) {
+    // console.log(e);
+    e.preventDefault();
+
+    // console.log(e.target.classList);
+    if (e.target.classList.contains('zoom-img')) {
+        const test = e.target.parentElement;
+        // console.log(test);
+        leerDatosCurso(test);
+        // re.style.pointerEvents = "none";
+    }
+
+    
+
+
+}
+
+
+function leerDatosCurso(curso) {
+    console.log(curso)
+    const infoImg = {
+        imagen: curso.querySelector('.zoom-img').src
+    };
+
+    insertarImg(infoImg);
+    
+}
+
+function insertarImg(infImg) {
+    console.log(infImg.imagen);
+    document.getElementsByTagName("html")[0].style.overflow = "hidden";
+    // console.log(re.parentNode);
+    caja.style.height = '2000' + "px";
+    caja.classList.remove('caja');
+    caja.classList.add('caja-off');
+    contenedor = document.createElement('div');
+    caja.appendChild(contenedor);
+    contenedor.classList.add('zoom');
+    cerrar = document.createElement('img');
+    cerrar.setAttribute('src', './images/close.svg');
+    cerrar.setAttribute('class', 'close');
+    contenedor.appendChild(cerrar);
+
+    let copyImgGif = document.createElement('div');
+    copyImgGif.innerHTML = `
+        <img src="${infImg.imagen}" class="zoom-img">
+    `
+    contenedor.appendChild(copyImgGif);
+
+
+
+
+    contenedor5 = document.createElement('div');
+    contenedor5.classList.add('container');
+    contenedor.appendChild(contenedor5);
+    contenedor3 = document.createElement('div');
+    contenedor3.classList.add('container-text');
+    contenedor5.appendChild(contenedor3)
+    text = document.createElement('p')
+    contenedor3.appendChild(text);
+    text = document.createElement('h3')
+    contenedor3.appendChild(text);
+    contenedor2 = document.createElement('div');
+    contenedor2.classList.add('container-img');
+    contenedor5.appendChild(contenedor2);
+    fav = document.createElement('img');
+    fav.setAttribute('src', './images/icon-fav-active.svg');
+    fav.setAttribute('class', 'img-fav');
+    contenedor2.appendChild(fav);
+    contenedor4 = document.createElement('div');
+    contenedor4.classList.add('box-img-download');
+    contenedor2.appendChild(contenedor4);
+    descarga = document.createElement('img');
+    descarga.setAttribute('src', './images/icon-download.svg');
+    descarga.setAttribute('class', 'img-download');
+    contenedor4.appendChild(descarga);
+
+    cerrar.addEventListener('click', function(ev){
+        // console.log(contenedor);
+        ev.preventDefault();
+        // console.log('cerrar');
+        // console.log(this.parentNode.parentNode);
+        // // console.log(this.parentNode);
+        // console.log(re);
+        // console.log(re.parentNode);
+        document.getElementsByTagName("html")[0].style.overflow = "auto";
+        caja.classList.remove('caja-off');
+        caja.classList.add('caja');
+        // re.style.pointerEvents = 'auto';
+        // padreImg.appendChild(re);
+        // console.log(re);
+        // console.log(re.parentNode);                    
+        this.parentNode.remove();
+        // contenedor.classList.add('error');
+        
+    })
 }
